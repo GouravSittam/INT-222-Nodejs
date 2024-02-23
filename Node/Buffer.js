3da8cded6e2023720a8f4926f14040c593ca9e3c
Gourav Chaudhary <gouravsittam@gmail.com>
2024-02-23 15:35:30 +0530

    update

--- Node/Buffer.js
+++ Node/Buffer.js
@@ -9,47 +9,66 @@
 
 //give it size of buffer
 
-var buff=Buffer.alloc(10)
-console.log(buff)
-buff.write("write")
-console.log(buff)
-var a=buff.toString()
-console.log(a);
+// var buff=Buffer.alloc(10)
+// console.log(buff)
+// buff.write("write")
+// console.log(buff)
+// var a=buff.toString()
+// console.log(a);
 
 
-//method 2
-var buff2=new Buffer('node')
-console.log(buff2)
+// //method 2
+// var buff2=new Buffer('node')
+// console.log(buff2)
 
-//method 3
-var buff3=Buffer.from([1,2,3,4,5,6,7])
-console.log(buff3)
+// //method 3
+// var buff3=Buffer.from([1,2,3,4,5,6,7])
+// console.log(buff3)
 
-// method 4 lengthcalculation
-var length = buff.length
-console.log(length)
+// // method 4 lengthcalculation
+// var length = buff.length
+// console.log(length)
 
-//method 5 concationtion
-console.log(Buffer.concat([buff,buff2,buff3]))
+// //method 5 concationtion
+// console.log(Buffer.concat([buff,buff2,buff3]))
 
-//copy buffer
-const buffersrc=new Buffer("ABC")
-const bufferDest=Buffer.alloc(3)
-buffersrc.copy(bufferDest)
-const Data=bufferDest.toString('utf-8')
-console.log(Data)
+// //copy buffer
+// const buffersrc=new Buffer("ABC")
+// const bufferDest=Buffer.alloc(3)
+// buffersrc.copy(bufferDest)
+// const Data=bufferDest.toString('utf-8')
+// console.log(Data)
 
 
-//slicing data
-const bufferold=new Buffer(
-    'nodeisthere toindroduceyou toindustry'
-)
-const buffernew=bufferold.slice(14,23)
-console.log(buffernew.toString())
+// //slicing data
+// const bufferold=new Buffer(
+//     'nodeisthere toindroduceyou toindustry'
+// )
+// const buffernew=bufferold.slice(14,23)
+// console.log(buffernew.toString())
 
-//compare 2 buffer
-const buffero=new Buffer("Grv")
-const buffert=new Buffer("Chaudhary")
-var result=buffero.compare(buffert)
-console.log(result)
+// //compare 2 buffer
+// const buffero=new Buffer("Grv")
+// const buffert=new Buffer("Chaudhary")
+// var result=buffero.compare(buffert)
+// console.log(result)
+
+                          //PATH
+
+// const path=require('path')
+// const directory=path.basename("C:\\Users\\hp\\OneDrive\\Desktop\\❤️❤️.jpeg")
+// console.log(directory)
+
+// const path=require('path')
+// const directory1=path.normalize("C:\\Users\\hp\\OneDrive\\Desktop\\❤️❤️.jpeg")
+// console.log(directory1)
+
+// const path=require('path')
+// const directory2=path.toNamespacedp("C:\\Users\\hp\\OneDrive\\Desktop\\❤️❤️.jpeg")
+// console.log(directory2)
+
+// const path=require('path')
+// console.log(path.delimiter)
+
+
 
--- Node/HTTPmodule.js
+++ Node/HTTPmodule.js
@@ -24,9 +24,9 @@
 // address.toJSON
 // console.log(address)
 
-var querystring = require('querystring');
-// var q = querystring.stringify({year:2017,month:2});
-// var q = querystring.escape({year:2017,month:2});
-// var q = querystring.unescape({year:2017,month:2});
-// var q = querystring.decode({year:2017,month:2});
-console.log(q);
+// var querystring = require('querystring');
+// // var q = querystring.stringify({year:2017,month:2});
+// // var q = querystring.escape({year:2017,month:2});
+// // var q = querystring.unescape({year:2017,month:2});
+// // var q = querystring.decode({year:2017,month:2});
+// console.log(q);
