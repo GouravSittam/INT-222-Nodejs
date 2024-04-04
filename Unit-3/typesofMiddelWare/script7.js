0ccb76023bdf8ac0ef0bdf0091540421601e1dfe
Gourav Chaudhary <gouravsittam@gmail.com>
2024-04-04 11:12:29 +0530

    new files

--- Unit-3/typesofMiddelWare/script7.js
+++ Unit-3/typesofMiddelWare/script7.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const session=require('express-session');
 const app=express();
-const port=8000;
+const port=1999;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(session({
@@ -11,20 +11,20 @@ app.use(session({
     saveUninitialized: true
 }));
 let user=[
-    {id: 1,username: 'Gourav',password: 'password1'},
-    {id: 2,username: 'Gouravc',password: 'password2'}
+    {id: 1,username: 'Gourav',password:'password1'},
+    {id: 2,username: 'Gouravc',password:'password2'}
 ];
 function authentication(req,res,next){
     if(req.session && req.session.user){
         return next();
     }else{
-        return res.redirect('login7');
+        return res.redirect('/login');
     }
 }
-app.get('/login7',(req,res)=>{
-    res.sendFile(__dirname+"/login7.html");
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+"/login.html");
 });
-app.post('/login7',(req,res)=>{
+app.post('/login',(req,res)=>{
     const { username,password}=req.body;
     const user=user.find(u => u.username === username && u.password=== password);
     if(user){
@@ -36,12 +36,12 @@ app.post('/login7',(req,res)=>{
 });
 app.get('/logout',(req,res)=>{
     req.session.destroy();
-    res.redirect('/login7');
+    res.redirect('/login');
 });
-app.get('/protected',authentication,(req,res) => {
-    res.send(`Welcome ${req.session.user.username}! this is a protected route. <a href="/logout">logout</a>`);
+app.get('/protected', authentication, (req, res) => {
+    res.send(`Welcome ${req.session.user.username}! This is a protected route. <a href="/logout">Logout</a>`);
 });
-app.listen(8000,()=>{
-    console.log(`Server is on listening port 8000`);
+app.listen(port, () => {
+    console.log(`Server is listening on port 8000`);
 });
 
